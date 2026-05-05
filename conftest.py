f7e0c088ec8d5be102151d1335a4876a66198b2e
Navya <navya@englishgurukul.in>
2026-05-05 19:07:44 +0530

    jenkins

--- conftest.py
+++ conftest.py
@@ -2,6 +2,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from pages.login_page import LoginPage
+import os
+from datetime import datetime
+import allure
 
 
 @pytest.fixture
@@ -26,15 +29,34 @@ def driver():
     yield driver
     driver.quit()
 
-
 @pytest.fixture
 def logged_in_driver(driver):
     login = LoginPage(driver)
     login.open()
     login.login("Sanskruthi School - Nalgonda", "8247282479")
-
     return driver
 
 
 def take_screenshot(driver, name="error"):
     driver.save_screenshot(f"{name}.png")
+    
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver", None)
+
+        if driver:
+            os.makedirs("reports", exist_ok=True)
+
+            file_name = f"reports/{item.name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+            driver.save_screenshot(file_name)
+
+            # ✅ Attach to Allure report
+            allure.attach.file(
+                file_name,
+                name="Screenshot on Failure",
+                attachment_type=allure.attachment_type.PNG
+            )
